bd6e1f5b3dbf8790ddcfab88a123bea2cfe9cfbe
Fiifi Blankson <phiiphiblankson@gmail.com>
2024-02-06 19:04:56 +0000

    for loop and if else else if completed

---
+++ loop.js
@@ -0,0 +1,22 @@
+let nameOfColleagues= ['Andrew', 'Mariama', 'Eric', 'Phoebe', 'Nii', 'Issah', 'Ninell', 'Micheal', 'Jason', 'Enam', 'Felix', 'Isaac']
+
+for (let i = 0; i<nameOfColleagues.length; i++) {
+    document.write(nameOfColleagues[i] +"<br>")
+};
+
+
+let shop =("groceries");
+
+if (shop == 'story books'){
+    console.log('parent buys story books');
+} else if (shop=='shoes'){
+    console.log('parent buy shoes');
+} else if (shop=='clothes'){
+console.log('parent buy clothes')
+} else if (shop=='groceries'){
+    console.log('parent buy groceries')
+} else if (shop=='drinks'){
+    console.log('parent buy drinks')
+}else{
+    console.log('no shopping')
+}
